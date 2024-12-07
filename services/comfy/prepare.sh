a4b0bc89dc0d3fdb7ae1ad444610cad366c91cee
elvinchan <elvinchan@foxmail.com>
2024-12-07 20:16:11 +0800

    feat: update comfy to v0.2.2

---
+++ services/comfy/prepare.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# manager
+pip install GitPython
+pip install PyGithub
+pip install matrix-client==0.4.0
+pip install transformers
+pip install huggingface-hub>0.20
+pip install typer
+pip install rich
+pip install typing-extensions
+# bilbox
+pip install colour-science>=0.4.3
+pip install numpy>=1.19.0
+pip install imageio>=2.31.0
+pip install opencv-python>=4.8.1.78
+# reactor
+pip install albumentations>=1.4.16
+pip install insightface==0.7.3
+pip install onnx>=1.14.0
+pip install segment_anything
+pip install ultralytics
+# Dynamic Prompts
+pip install dynamicprompts==0.30.2
+# GGUF
+pip install gguf>=0.9.1
+# Easy
+pip install diffusers
+pip install accelerate
+pip install clip_interrogator>=0.6.0
+pip install lark-parser
+pip install onnxruntime
+pip install sentencepiece
+pip install spandrel
+# x-flux-comfyui
+pip install GitPython
+pip install einops==0.8.0
+pip install transformers
+pip install diffusers
+pip install sentencepiece
+pip install opencv-python
+# controlnet_aux
+pip install importlib_metadata
+pip install scipy
+pip install opencv-python>=4.7.0.72
+pip install filelock
+pip install Pillow
+pip install einops
+pip install torchvision
+pip install pyyaml
+pip install scikit-image
+pip install python-dateutil
+pip install mediapipe
+pip install svglib
+pip install fvcore
+pip install yapf
+pip install omegaconf
+pip install ftfy
+pip install addict
+pip install yacs
+pip install trimesh[easy]
+pip install scikit-learn
+pip install matplotlib
+# extra
+pip install onnxruntime-gpu
